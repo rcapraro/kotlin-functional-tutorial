10de6042f28d944863ea43697b9fc21a4d0c93ef
Richard Capraro <richard.capraro@gmail.com>
2019-10-05 21:22:18 +0200

    Upgrade to arrow 0.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    id("org.jetbrains.kotlin.jvm").version("1.3.41")
+    kotlin("jvm") version "1.3.50"
 }
 
-val arrowVersion = "0.9.1-SNAPSHOT"
+val arrowVersion = "0.10.0"
 
 repositories {
+    mavenCentral()
     jcenter()
-    maven("https://oss.jfrog.org/artifactory/oss-snapshot-local/")
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("io.arrow-kt:arrow-core-data:$arrowVersion")
-    implementation("io.arrow-kt:arrow-core-extensions:$arrowVersion")
+    implementation("io.arrow-kt:arrow-core:$arrowVersion")
     implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
-    implementation("io.arrow-kt:arrow-typeclasses:$arrowVersion")
-    implementation("io.arrow-kt:arrow-extras-data:$arrowVersion")
-    implementation("io.arrow-kt:arrow-extras-extensions:$arrowVersion")
     implementation("io.arrow-kt:arrow-fx:$arrowVersion")
-    implementation("io.arrow-kt:arrow-effects-data:$arrowVersion")
-    implementation("io.arrow-kt:arrow-effects-extensions:$arrowVersion")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
